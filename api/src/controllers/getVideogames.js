6a7df8ded2587f0635018b9c228a56424c4c8994
Pablo Szejpiacki <molder26@hotmail.com>
2022-07-19 21:13:26 -0300

    Fix getVideogames for loop URL page

--- api/src/controllers/getVideogames.js
+++ api/src/controllers/getVideogames.js
@@ -5,26 +5,33 @@ const { API_KEY } = process.env;
 
 exports.getVideogames = async (req, res) => {
     let allVideoGames = [];
+    let nextUrl = URL;
 
-    for (let i = 0; i < 5; i++) {
-        const { data } = await axios
-            .get(`${URL}/games?key=${API_KEY}`)
-            .catch((error) => res.status(400).json(error));
-
-        const videoGames = data.results.map((game) => {
-            allVideoGames.push( {
-                id: game.id,
-                name: game.name,
-                background_image: game.background_image,
-                genres: game.genres && game.genres.map((g) => g.name).filter(g => g != null).join(', '),
-                // rating: game.rating,
-                // released: game.released,
-                // platforms: game.parent_platforms && game.parent_platforms.map((p) => p.platform.name).filter(p => p != null).join(', '),
+    for (let i = 1; i < 6; i++) {
+        try {
+            const { data } = await axios
+                .get(`${nextUrl}/games?key=${API_KEY}&page=${i}`)
+                .catch((error) => res.status(400).json(error));
+    
+            console.log(nextUrl);
+            nextUrl = data.next;
+            console.log(data.next);
+    
+            const videoGames = data.results.map((game) => {
+                allVideoGames.push( {
+                    id: game.id,
+                    name: game.name,
+                    background_image: game.background_image,
+                    genres: game.genres && game.genres.map((g) => g.name).filter(g => g != null).join(', '),
+                    // rating: game.rating,
+                    // released: game.released,
+                    // platforms: game.parent_platforms && game.parent_platforms.map((p) => p.platform.name).filter(p => p != null).join(', '),
+                });
             });
-        });
+        } catch (error) {
+            res.send(error)
+        }
     }
-
     console.log(allVideoGames.length);
-
     return res.status(200).send(allVideoGames);
 };
