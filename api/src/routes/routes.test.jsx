c972b923eb06dfb88193ea63b7098a7788f0f4be
Pablo Szejpiacki <molder26@hotmail.com>
2022-08-13 20:23:59 -0300

    Split test in routes

--- api/src/routes/routes.test.jsx
+++ api/src/routes/routes.test.jsx
@@ -2,15 +2,23 @@ const request = require("supertest");
 const express = require("express");
 const router = require("./index.js");
 const app = express();
-const { Videogame, conn } = require("../db.js");
 
 app.use("/", router);
 
-describe("Good Home Routes", function () {
-    test("responds to /videogames", async () => {
-        const res = await request(app).get("/videogames");
-        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-        expect(res.statusCode).toBe(200);
-        expect(res._body.length).toBeGreaterThan(99);
+describe("Testing Routes", function () {
+    let res;
+
+    beforeAll(async () => {
+        res = await request(app).get("/videogames");
     }, 30000);
+
+    test("responds to /videogames and return a json", () => {
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+    });
+    test("responds to /videogames and return a status 200", () => {
+        expect(res.statusCode).toBe(200);
+    });
+    test("responds to /videogames and return 100 videogames from api", () => {
+        expect(res._body.length).toBeGreaterThan(99);
+    });
 });
